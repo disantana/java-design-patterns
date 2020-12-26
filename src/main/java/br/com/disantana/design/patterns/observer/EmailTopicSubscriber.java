6a3ac0bccf56dc476920cf73644db23b4fdaaa69
Diego de Santana Pereira <diego.spereira-ext@viavarejo.com.br>
2020-12-26 17:21:01 -0300

    feat: Atualiza os métodos da classe EmailTopicSubscriber

--- src/main/java/br/com/disantana/design/patterns/observer/EmailTopicSubscriber.java
+++ src/main/java/br/com/disantana/design/patterns/observer/EmailTopicSubscriber.java
@@ -2,17 +2,24 @@ package br.com.disantana.design.patterns.observer;
 
 public class EmailTopicSubscriber implements Observer {
 
-    EmailTopicSubscriber() {
+    private String message;
+    private Subject topic;
 
+    EmailTopicSubscriber(String message) {
+        this.message = message;
     }
 
     @Override
     public void update() {
-
+        String message = (String) topic.getUpdate(this);
+        if (message == null)
+            System.out.println(this.message + " no new message on this topic!");
+        else
+            System.out.println(this.message +" Retrieving message: " + message);
     }
 
     @Override
     public void setSubject(Subject subject) {
-
+        this.topic = subject;
     }
 }
