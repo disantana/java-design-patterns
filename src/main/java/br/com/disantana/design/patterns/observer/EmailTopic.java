cdd4fe16a806662743f784362e06f579b70160e7
Diego de Santana Pereira <diego.spereira-ext@viavarejo.com.br>
2020-12-26 15:49:39 -0300

    feat: Atualiza a classe EmailTopic com a implementação dos métodos e adiciona os atributos message e observers;
    Atualiza a assinatura dos métodos notifyObserver e getUpdate.

--- src/main/java/br/com/disantana/design/patterns/observer/EmailTopic.java
+++ src/main/java/br/com/disantana/design/patterns/observer/EmailTopic.java
@@ -1,28 +1,41 @@
 package br.com.disantana.design.patterns.observer;
 
+import java.util.List;
+
 public class EmailTopic implements Subject {
 
-    EmailTopic () {
+    private List<Observer> observers;
+    private String message;
 
+    EmailTopic (List<Observer> observers) {
+        this.observers = observers;
     }
 
     @Override
     public void register(Observer observer) {
-
+        if (observers == null) throw new NullPointerException("Null object/observer");
+        if (!observers.contains(observer))
+            observers.add(observer);
     }
 
     @Override
     public void unregister(Observer observer) {
-
+        observers.remove(observer);
     }
 
     @Override
-    public void notifyObserver(Observer observer) {
-
+    public void notifyObserver() {
+        observers.forEach(observer -> observer.update());
     }
 
     @Override
-    public void getUpdate(Observer observer) {
+    public Object getUpdate(Observer observer) {
+        return this.message;
+    }
 
+    public void postMessage(String message) {
+        System.out.println("Message posted to my topic : " + message);
+        this.message = message;
+        notifyObserver();
     }
 }
--- src/main/java/br/com/disantana/design/patterns/observer/Subject.java
+++ src/main/java/br/com/disantana/design/patterns/observer/Subject.java
@@ -3,6 +3,6 @@ package br.com.disantana.design.patterns.observer;
 interface Subject {
     void register(Observer observer);
     void unregister(Observer observer);
-    void notifyObserver(Observer observer);
-    void getUpdate(Observer observer);
+    void notifyObserver();
+    Object getUpdate(Observer observer);
 }
